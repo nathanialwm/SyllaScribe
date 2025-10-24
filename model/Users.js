b240334b4f764ab596109c0f89177f0a010217fa
Nathanial Martin <nathanialwm@gmail.com>
2025-10-24 15:11:08 -0400

    Setup Schemas for database

---
+++ model/Users.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const {Schema, model} = mongoose;
+
+const userSchema = new Schema({
+    userId: {type: Number, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    name: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+    settings: [{
+        darkMode: {type: Boolean, default: false},
+        notifications: {type: Boolean, default: true}, 
+    }]
+});
+
+export default model('User', userSchema);
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb+srv://admin:418Yadmin@scribecluster.ts78qln.mongodb.net/?appName=ScribeCluster');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
